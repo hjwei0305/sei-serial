b3d3254cecb5bc4d32b80f8202504013e430812d
liusonglin <songlin.liu@changhong.com>
2020-02-21 15:03:42 +0800

    条码管理查询

--- sei-serial-service/src/main/java/com/changhong/sei/serial/controller/SerialNumberConfigController.java
+++ sei-serial-service/src/main/java/com/changhong/sei/serial/controller/SerialNumberConfigController.java
@@ -3,14 +3,19 @@ package com.changhong.sei.serial.controller;
 import com.changhong.sei.core.dto.ResultData;
 import com.changhong.sei.core.dto.serach.PageResult;
 import com.changhong.sei.core.dto.serach.Search;
+import com.changhong.sei.core.dto.serach.SearchFilter;
 import com.changhong.sei.core.service.bo.OperateResultWithData;
+import com.changhong.sei.serial.entity.BarCodeAssociate;
 import com.changhong.sei.serial.entity.SerialNumberConfig;
 import com.changhong.sei.serial.entity.dto.BarCodeDto;
 import com.changhong.sei.serial.entity.enumclass.ConfigType;
+import com.changhong.sei.serial.sdk.BarCodeService;
+import com.changhong.sei.serial.service.BarCodeAssociateService;
 import com.changhong.sei.serial.service.SerialNumberConfigService;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("serialNumberConfig")
@@ -18,6 +23,9 @@ public class SerialNumberConfigController {
 
     @Autowired
     private SerialNumberConfigService serialNumberConfigService;
+
+    @Autowired
+    private BarCodeAssociateService barCodeAssociateService;
 
     @PostMapping("save")
     public ResultData<SerialNumberConfig> save(@RequestBody SerialNumberConfig serialNumberConfig){
@@ -46,4 +54,30 @@ public class SerialNumberConfigController {
     public String genNumberAndSaveAssociate(@RequestBody BarCodeDto barCodeDto){
       return serialNumberConfigService.genNumberAndSaveAssociate(barCodeDto);
     }
+
+    @GetMapping("getReferenceIdByBarCode")
+    public ResultData<BarCodeAssociate> getReferenceIdByBarCode(@RequestParam String barCode){
+        BarCodeAssociate barCodeAssociate = barCodeAssociateService.findByProperty("barCode",barCode);
+        return ResultData.success(barCodeAssociate);
+    }
+
+    @GetMapping("getBarCodeListByReferenceId")
+    public ResultData<List<BarCodeAssociate>> getBarCodeListByReferenceId(@RequestParam String referenceId){
+        List<BarCodeAssociate> barCodeAssociates = barCodeAssociateService.findListByProperty("referenceId",referenceId);
+        return ResultData.success(barCodeAssociates);
+    }
+
+    @GetMapping("getReferenceIdsByListBarCode")
+    public ResultData<List<BarCodeAssociate>> getReferenceIdsByListBarCode(@RequestBody List<String> barCodes){
+        SearchFilter searchFilter = new SearchFilter();
+        searchFilter.setFieldName("barCode");
+        searchFilter.setOperator(SearchFilter.Operator.IN);
+        searchFilter.setValue(barCodes);
+        List<BarCodeAssociate> barCodeAssociates = barCodeAssociateService.findByFilter(searchFilter);
+        return ResultData.success(barCodeAssociates);
+    }
+
+
+
+
 }
